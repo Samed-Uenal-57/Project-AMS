916cfcc098b9cfeaef713b14e8097b19d1aa8e91
Samed Uenal <samed5755@gmail.com>
2025-11-25 14:31:44 +0100

    Server funktioniert

--- frontend/server.js
+++ frontend/server.js
@@ -3,13 +3,16 @@ const path = require('path');
 const app = express();
 const PORT = 5500;
 
+
+const FRONTPATH = path.join(__dirname, 'frontend');
 // Statische Dateien ausliefern
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(FRONTPATH));
 
 // SPA: Alle Anfragen auf index.html umleiten
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index/index.html'));
+  res.sendFile(path.join(__dirname,'index', 'index.html'));
 });
+
 
 app.listen(PORT, () => {
   console.log(`Frontend läuft auf http://localhost:${PORT}`);
