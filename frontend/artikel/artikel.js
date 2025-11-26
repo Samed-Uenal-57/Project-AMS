5f9357b117a1509c6bc9247a6f4e65e6e7210fee
Samed-Uenal-57 <uenalsam@hs-albsig.de>
2025-11-26 11:35:17 +0100

    Server Changes

--- frontend/artikel/artikel.js
+++ frontend/artikel/artikel.js
@@ -21,10 +21,10 @@ document.addEventListener("DOMContentLoaded", () =>{
                     <p>${p.beschreibung}</p>
                     <label for="extra">Zusatzoptionen:</label>
                     <br>
+                    <input type="radio" id="extra2" name="extra" value="keine" checked>
+                    <label for="check">Keine</label><br>
                     <input type="radio" id="extra1" name="extra" value="beeren">
                     <label for="check">Schmetterlinge (essbar)</label><br>
-                    <input type="radio" id="extra2" name="extra" value="keine">
-                    <label for="check">Keine</label><br>
                     <label for="piece">Stückzahl:</label>
                     <input type="number" id="piece" name="piece" value="1" min="1" step="1"> <br>
                     <button class="button">In den Warenkorb</button>
@@ -34,11 +34,28 @@ document.addEventListener("DOMContentLoaded", () =>{
         })
         .catch(err => console.error("Fehler beim Laden: ", err));
 
-    fetch(`http://localhost:8000/api/produkt/alle`)
-        .then(res=> res.json())
-        .then(p=> {
-            
+    
+    const container2 = document.getElementById("gallery");
+    
+    fetch("http://localhost:8000/api/produkt/alle")
+    .then(res=> res.json())
+    .then(produkte => {
+        const top = produkte.slice(0,4);
+        top.forEach(p=>{
+            const div1 = document.createElement("div");
+            const bildPfad = 'http://localhost:8000/' + p.bilder[0].bildpfad
+                
+            div1.className = "einkauf";
+            div1.innerHTML = `
+                <a href="../artikel/artikel.html?id=${p.id}">
+                <img src= "${bildPfad}" alt="${p.bezeichnung}">
+                <h2>${p.bezeichnung}</h2>
+                <p>Preis: ${p.preis} € </p>
+            `;
+            container2.appendChild(div1)
         })
+    })
+    .catch(err => console.error("Fehler beim Laden: ", err));
 });
 
 
